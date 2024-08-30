1dc05670e7afa379c04949b6f4fafdc85869d70b
Jonathyn Stiverson <jstiverson@alvine.com>
2024-08-30 14:04:34 -0500

    feat: add custom PowerShell prompt

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -6,3 +6,31 @@ function gitc { git commit --verbose @args }
 function gitps { git push @args }
 function gitpu { git pull @args }
 function gita { git add @args }
+
+# Set the PowerShell prompt
+function prompt
+{
+    # Set the window title to the full path, so that way we aren't taking up so much space with 
+    #   with the path.
+    $Host.UI.RawUI.WindowTitle = Get-Location
+
+
+    $parentDirectory = Get-Location `
+        | Split-Path -Parent -ErrorAction SilentlyContinue `
+        | Split-Path -Leaf -ErrorAction SilentlyContinue
+
+    If ($parentDirectory) {
+        Write-Host "$parentDirectory\" -NoNewLine
+    }
+
+    $currentDirectory = Get-Location | Split-Path -Leaf
+    Write-Host "$currentDirectory" -NoNewLine
+
+    $arrows = '>'
+    if ($NestedPromptLevel -gt 0) { $arrows *= (1 + $NestedPromptLevel) }
+    Write-Host "$arrows" -NoNewLine
+
+    # For some reason, the PowerShell prompt appends "PS " to the end if this isn't 
+    #   provided.
+    ' '
+}
