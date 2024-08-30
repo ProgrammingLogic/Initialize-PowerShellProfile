017812bc799437b9598ebc8c314689571de59f5e
Jonathyn Stiverson <jstiverson@alvine.com>
2024-08-30 14:35:06 -0500

    fix: rename gitps to gitp

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -3,7 +3,7 @@
 # We have to define a function and pass args into it, because Set-Alias does not support
 # passing arguments into commands.
 function gitc { git commit --verbose @args }
-function gitps { git push @args }
+function gitp { git push @args }
 function gitpu { git pull @args }
 function gita { git add @args }
 
