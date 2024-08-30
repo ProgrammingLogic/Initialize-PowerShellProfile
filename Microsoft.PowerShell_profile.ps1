7fa6147a95aefae070de81e90dca110903b3b074
Jonathyn Stiverson <jstiverson@alvine.com>
2024-08-30 11:34:41 -0500

    feat: add initial profile

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,5 @@
+# Aliases
+Set-Alias -Name gitc -Value "git commit --verbose"
+Set-Alias -Name gitps -Value "git push"
+Set-Alias -Name gitpu -Value "git pull"
+Set-Alias -Name gita -Value "git add"
